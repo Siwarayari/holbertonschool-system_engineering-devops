e97724a7ccab3f7ef7a9c7738e264441da1e4100
Siwar <siwarayari1999@gmail.com>
2022-05-16 15:07:43 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """
-script that, for a given employee ID, returns information about his/her
+Module that returns information
+    about his/her todo list progress
 """
-import requests
-from sys import argv
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    req1 = requests.get(url + "users/" + argv[1])
-    employee = req1.json().get("name")
-    req2 = requests.get(url + 'todos?userId=' + argv[1]).json()
-    completed_tasks = 0
-    all_tasks = 0
-    title = []
-    if employee is not None:
-        for i in req2:
-            if i.get("completed") is True:
-                completed_tasks += 1
-                title.append(i.get("title"))
-            all_tasks += 1
-        print(
-            "Employee {} is done with tasks({}/{}):"
-            .format(employee, completed_tasks, all_tasks)
-            )
-        for j in title:
-            print("\t {}".format(j))
+import requests
+import sys
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    name = user.json().get('name')
+    req = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = list(filter(lambda x: x['userId'] == int(sys.argv[1]), req.json()))
+    comp = list(filter(lambda x: x['completed'], todos))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, len(comp), len(todos)))
+    t = list(x['title'] for x in comp)
+    print("\n".join("\t {}".format(task) for task in t))
